5a2f113352efbe8ce623c619f307e335af723e60
0xinhua <wuxinhua.cn@gmail.com>
2023-10-08 17:17:59 +0800

    fix(footer): Change footer signature

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <Container>
         <div className="flex flex-col lg:flex-col items-center border-t border-neutral-200 py-4 text-gray-600">
           <div className="flex flex-col lg:flex-col justify-center items-center">
-            <p>{`Made with ❤️ by`}<a href='https://github.com/0xinhua/' target={"_blank"} className="text-blue-500 hover:underline underline-offset-4 hover:text-blue-600  duration-200 transition-colors ml-1">@{CMS_AUTHOR}</a> ©{currentYear}</p>
+            <p>{`Made with ❤️ and ☕️ by`}<a href='https://github.com/0xinhua/' target={"_blank"} className="text-blue-500 hover:underline underline-offset-4 hover:text-blue-600  duration-200 transition-colors ml-1">@{CMS_AUTHOR}</a> ©{currentYear}</p>
             <p>Powered by
               <a href="https://vercel.com/" className="text-blue-500 underline-offset-4 hover:text-blue-600 hover:underline  duration-200 transition-colors ml-1" target={"_blank"}>Vercel</a>
             </p>
