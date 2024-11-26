47df2e7b4036afd58f4f735ace30ea2af6408a1c
0xinhua <wuxinhua.cn@gmail.com>
2024-11-26 13:14:43 +0800

    Update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -25,14 +25,25 @@ export default function About () {
         </Head>
         <div>
           <div className="mb-4 inline-block">
-            <a href="https://321letter.substack.com/">
-              <Image alt="321来信" className="rounded mb-5" src="/assets/site/321-screenshot-20231128.jpeg" width={670} height={420} />
-            </a>
-              <p className="text-center mt-4 mb-2">👋 欢迎你访问我的网络日志。点击订阅 Newsletter - <a className="text-blue-500 dark:text-blue-200  hover:underline hover:text-blue-600 underline-offset-4" target="_blank" href="https://321letter.substack.com/" title="Substack - 321来信">321来信</a></p>
-              <p className="text-center">与超过 <span className="font-semibold"> 413 </span>🎉 位订阅读者一起关注 AI 、创业以及互联网最新的资讯和思考。</p>
+            <p className="text-center mt-4 mb-2">点击订阅 Newsletter - <a className="text-blue-500 dark:text-blue-200 font-semibold  hover:underline hover:text-blue-600 underline-offset-4" target="_blank" href="https://321letter.substack.com/" title="Substack - 321来信">321来信</a></p>
+            <p className="text-center">与超过 <span className="font-semibold"> 413 </span>🎉 位订阅读者一起关注 AI 、创业以及互联网最新的资讯和思考。</p>
+              <a href="https://321letter.substack.com/">
+                <Image alt="321来信" className="rounded" src="/assets/site/321-screenshot-20231128.jpeg" width={670} height={420} />
+              </a>
+          </div>
+          <div className="mb-4 inline-block">
+            <p className="text-center mb-2">
+              <a className="text-blue-500 dark:text-blue-200  hover:underline hover:text-blue-600 underline-offset-4 font-semibold" target="_blank" href="https://twittershots.com/" title="twitter screenshots generator">
+              TwitterShots.com </a>
+             - Making Pretty Twitter Screenshots.
+            </p>
+              <a href="https://twittershots.com/">
+                <Image alt="free twitter screenshot tool, download and share quickly" className="rounded mb-5 p-4 sm:p-9" src="/assets/site/twitter-screenshots-generator.png" width={670} height={420} />
+              </a>
           </div>
           <div className="text-center">
-            <p className="mb-2"> 0xinhua 之前在
+            <p className="mb-2">👋 欢迎你访问我的网络日志。</p>
+            <p className="mb-2">0xinhua 之前在
             <Link className="text-blue-500 dark:text-blue-200  hover:underline hover:text-blue-600 underline-offset-4" href="https://bytedance.com/" target="_blank"> ByteDance </Link>和
             <Link className="text-blue-500 dark:text-blue-200  hover:underline hover:text-blue-600 underline-offset-4" href={`https://juejin.cn/`} target="_blank"> juejin.cn </Link>写代码，目前在创业公司做全栈开发。</p>
             <p>只要命运齿轮沒有出差错，这个日志会一直更下去。</p>
